7a484eefbd58970fab32909ec5c952e346b6c642
Ram Singla <ram.singla@gmail.com>
2009-12-28 11:00:04 +0530

    ActionMailer Added

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,7 +4,7 @@ class Notifier < ActionMailer::Base
   
   def password_reset_instructions( user )  
     subject       "Password Reset Instructions"  
-    from          "Jurnalo Accounts Services"  
+    from          "Jurnalo.com Accounts Services <no-reply@jurnalo.com>"  
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url( user.perishable_token )  
@@ -12,7 +12,7 @@ class Notifier < ActionMailer::Base
   
   def account_activation_instructions( user )  
     subject       "Account Activiation Instructions"
-    from          "Jurnalo Accounts Services"
+    from          "Jurnalo.com Accounts Services <no-reply@jurnalo.com>"
     recipients    user.email
     sent_on       Time.now
     body          :account_activation_url => account_activation_url( user.perishable_token )
