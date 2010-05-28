e3cebdb5b4c92374bdfa43c591bcaa4bfa2528c9
Ram Singla <ram.singla@gmail.com>
2010-05-28 19:16:33 +0530

    Recurly Plugin Removed (Not Required)

--- vendor/plugins/recurly-client-ruby/lib/recurly/invoice.rb
+++
@@ -1,9 +0,0 @@
-module Recurly
-  class Invoice < RecurlyBase
-    self.element_name = "invoice"
-  
-    def self.list(account_code)
-      Invoice.find(:all, :from => "/accounts/#{account_code}/invoices")
-    end
-  end
-end
--- vendor/plugins/recurly-client-ruby/lib/recurly/transaction.rb
+++
@@ -1,6 +0,0 @@
-module Recurly
-  class Transaction < RecurlyBase
-    self.element_name = "transaction"
-    self.prefix = "/"  
-  end
-end
